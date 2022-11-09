c21753428375e336b34bf6340395f7bbd499a4b5
changleilei <852324033@qq.com>
2022-11-09 14:31:05 +0800

    add logging file

--- scheduler_server/logger.py
+++ scheduler_server/logger.py
@@ -7,14 +7,17 @@ import logging
 import sys
 
 
-def get_logger():
-    logger = logging.getLogger('gpt dialogue')
+def get_logger(name, filename='generate.log'):
+    logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     fmt = logging.Formatter('%(asctime)s ｜ %(levelname)s ｜ %(filename)s ｜ %(funcName)s ｜ %(lineno)s ｜ %(message)s')
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(fmt)
-
+    fh = logging.FileHandler(filename=f'logs/{filename}', encoding='utf-8', mode='a')
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(fmt)
     logger.addHandler(stream_handler)
+    logger.addHandler(fh)
 
     return logger
