0366e2684a513ed7d62b3a8006b63e35883dacfb
changleilei <852324033@qq.com>
2022-11-15 18:22:47 +0800

    英文判断脚本

--- utils.py
+++ utils.py
@@ -14,3 +14,15 @@ def judge_zh(sentence):
     else:
         return False
 
+def judge_en(sentence):
+    # 判断英文
+    ignores = []
+    result = ''.join(re.findall(r'[A-Za-z]', sentence))
+    result = result.lower()
+    for ignore in ignores:
+        if ignore in result:
+            result = result.replace(ignore, "")
+    if len(result) > len(sentence) / 2:
+        return True
+    else:
+        return False
